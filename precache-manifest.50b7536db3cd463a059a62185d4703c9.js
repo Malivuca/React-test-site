d61a51400db37838663bc87cf4a003fdf73e1b6b
Malivuca <miguelvargasdemarque@gmail.com>
2019-12-19 14:38:32 -0300

    Updates

---
+++ precache-manifest.50b7536db3cd463a059a62185d4703c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390c88bec4bbdd5fa6fba8ab5e82e197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632c8e0ab3331001ce50",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64ef9f9ea8a08ebcd8eb",
+    "url": "/static/js/2.c588ade7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c588ade7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "632c8e0ab3331001ce50",
+    "url": "/static/js/main.cc7f51fc.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
